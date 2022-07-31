62b7c9c3fc665d702b4518bfa185dfe6c58a29d2
Milad Nasiri <milad@ecognitionlabs.com>
2022-07-31 22:57:37 +0430

    set header content-type in axios

--- src/utils/http-helper.ts
+++ src/utils/http-helper.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { Observable, Observer } from "rxjs";
 
-const rootUrl = 'http://api.milad-nasiri.ir/api';
+const rootUrl = "http://api.milad-nasiri.ir/api";
 
 export const httpGet = (url: string): Observable<any> => {
   return new Observable((obs: Observer<any>) => {
     axios
-      .get(rootUrl + url)
+      .get(rootUrl + url, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
       .then((res: any) => {
         obs.next(res.data);
       })
@@ -19,7 +23,11 @@ export const httpGet = (url: string): Observable<any> => {
 export const httpPost = (url: string, params: any): Observable<any> => {
   return new Observable((obs: Observer<any>) => {
     axios
-      .post(rootUrl + url, params)
+      .post(rootUrl + url, params, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
       .then((res: any) => {
         obs.next(res.data);
       })
@@ -30,7 +38,7 @@ export const httpPost = (url: string, params: any): Observable<any> => {
 };
 
 export interface RespModel<T> {
-  data?: T,
-  message?: string,
-  error?: boolean
+  data?: T;
+  message?: string;
+  error?: boolean;
 }
