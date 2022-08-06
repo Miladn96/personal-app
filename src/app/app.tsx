26c19645728c98e56d6e9d2e1031f7bc19576dce
Milad Nasiri <milad@ecognitionlabs.com>
2022-08-06 23:44:33 +0430

    redirect and fix deploy file

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,34 +1,41 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 import { routes, RoutesModel } from "../routes";
 import "./style.scss";
 
 export const App = () => {
   const routeChildren = (_routes: RoutesModel[]) => (
     <>
-      {_routes.map((route) =>
-        route.children ? (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<route.component />}
-          >
-            {routeChildren(route.children)}
-          </Route>
-        ) : (
+      {_routes.map((route) => {
+        console.log(route)
+        if (route.redirectTo)
+          return (
+            <Route key={route.path} path={route.path} element={<Navigate to={route.redirectTo} replace />} />
+          );
+        if (route.children)
+          return (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={<route.component />}
+            >
+              {routeChildren(route.children)}
+            </Route>
+          );
+        return (
           <Route
             key={route.path}
             path={route.path}
             element={<route.component />}
           />
-        )
-      )}
+        );
+      })}
     </>
   );
   return (
     <>
-      <Link to={'/home/about'}>About</Link>
-      <br/>
-      <Link to={'/admin/messages'}>Admin</Link>
+      <Link to={"/home/about"}>About</Link>
+      <br />
+      <Link to={"/admin/messages"}>Admin</Link>
       <Routes>{routeChildren(routes)}</Routes>
     </>
   );
